0ff7ddde446fd907db03e664f111ec19a69b94c2
Sebastian Felis <sebafelis@hotmail.com>
2021-07-19 12:46:57 +0200

    Move backoffice cache to separate project

--- src/RoboLynx.Umbraco.QRCodeGenerator.Cache.Local/QRCodeGeneratorCacheComposer.cs
+++ src/RoboLynx.Umbraco.QRCodeGenerator.Cache.Local/QRCodeGeneratorCacheComposer.cs
@@ -8,7 +8,7 @@ using Umbraco.Core.Exceptions;
 using Umbraco.Core.IO;
 using Umbraco.Core.Logging;
 
-namespace RoboLynx.Umbraco.QRCodeGenerator.Cache
+namespace RoboLynx.Umbraco.QRCodeGenerator.Cache.Local
 {
     [ComposeBefore(typeof(QRCodeGeneratorCoreComposer))]
     public class QRCodeGeneratorCacheComposer : IUserComposer
@@ -22,7 +22,7 @@ namespace RoboLynx.Umbraco.QRCodeGenerator.Cache
 
             if (!config.Disable)
             {
-                composition.Register<IQRCodeCache>(f=> new QRCodeCache<IBackofficeQRCodeCache>(CacheName,
+                composition.RegisterFor<IQRCodeCache, IBackofficeQRCodeCache>(f=> new QRCodeCache<IBackofficeQRCodeCache>(CacheName,
                     f.GetInstance<AppCaches>(), new QRCodeCacheFileSystem(new PhysicalFileSystem(config.CacheLocation), 
                     TimeSpan.FromDays(config.MaxDays), f.GetInstance<ILogger>(), f.GetInstance<IDateTimeOffsetProvider>()), 
                     null, f.GetInstance<IRuntimeState>(), 
