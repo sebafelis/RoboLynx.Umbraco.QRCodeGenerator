67334b1ac9df0c82de3a8733a6a6c9e07ac40ae7
Sebastian Felis <sebafelis@hotmail.com>
2021-07-22 17:39:49 +0200

    Fix IQRCodeService extensions

--- src/RoboLynx.Umbraco.QRCodeGenerator.Frontend/QRCodeServiceExtensions.cs
+++ src/RoboLynx.Umbraco.QRCodeGenerator.Frontend/QRCodeServiceExtensions.cs
@@ -6,17 +6,17 @@ namespace RoboLynx.Umbraco.QRCodeGenerator
 {
     public static class QRCodeServiceExtensions
     {
-        public static Stream GetStream(this QRCodeService service, QRCode property, string culture, QRCodeSettings settings)
+        public static Stream GetStream(this IQRCodeService service, QRCode property, string culture, QRCodeSettings settings)
         {
             return service.GetStream(property.PublishedContent, property.PropertyAlias, culture, settings, property.CacheName);
         }
 
-        public static Stream GetStream(this QRCodeService service, QRCode property, string culture, QRCodeSettings settings, string cacheName)
+        public static Stream GetStream(this IQRCodeService service, QRCode property, string culture, QRCodeSettings settings, string cacheName)
         {
             return service.GetStream(property.PublishedContent, property.PropertyAlias, culture, settings, cacheName);
         }
 
-        public static QRCodeSettings GetDefaultSettings(this QRCodeService service, QRCode property)
+        public static QRCodeSettings GetDefaultSettings(this IQRCodeService service, QRCode property)
         {
             return service.GetDefaultSettings(property.PublishedContent, property.PropertyAlias);
         }
