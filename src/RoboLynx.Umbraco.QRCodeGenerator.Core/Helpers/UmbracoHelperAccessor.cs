3c5fe6a61d72c98b95c514132c54fdd01d30a55f
Sebastian Felis <sebafelis@hotmail.com>
2021-12-28 23:22:14 +0100

    Add UmbracoHelperAccessor

---
+++ src/RoboLynx.Umbraco.QRCodeGenerator.Core/Helpers/UmbracoHelperAccessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Web;
+using Composing = Umbraco.Web.Composing;
+
+namespace RoboLynx.Umbraco.QRCodeGenerator.Helpers
+{
+    public interface IUmbracoHelperAccessor
+    {
+        bool TryGetUmbracoHelper(out UmbracoHelper umbracoHelper);
+    }
+
+    public class UmbracoHelperAccessor : IUmbracoHelperAccessor
+    {
+
+        public bool TryGetUmbracoHelper(out UmbracoHelper umbracoHelper)
+        {
+            umbracoHelper = Composing.Current.UmbracoHelper;
+            return umbracoHelper is not null;
+        }
+    }
+}
