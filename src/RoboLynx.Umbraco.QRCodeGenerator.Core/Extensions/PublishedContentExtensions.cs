303b40b0bd9607c517f29707b307d0fcde7a1f51
Sebastian Felis <sebafelis@hotmail.com>
2021-12-28 20:25:55 +0100

    Add GetUdi extension method to IPublishedContent

---
+++ src/RoboLynx.Umbraco.QRCodeGenerator.Core/Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace RoboLynx.Umbraco.QRCodeGenerator
+{
+    public static class PublishedContentExtensions 
+    {
+        public static Udi GetUdi(this IPublishedContent publishedContent)
+        {
+            var itemType = publishedContent.ItemType;
+            string entityType;
+
+            switch (itemType)
+            {
+                case PublishedItemType.Content: 
+                    entityType = UmbracoObjectTypes.Document.GetUdiType();
+                    break;
+                case PublishedItemType.Media:
+                    entityType = UmbracoObjectTypes.Media.GetUdiType();
+                    break;
+                case PublishedItemType.Member:
+                    entityType = UmbracoObjectTypes.Member.GetUdiType();
+                    break;
+                default:
+                    entityType = UmbracoObjectTypes.Unknown.GetUdiType();
+                    break;
+            }
+
+            var udi = Udi.Create(entityType, publishedContent.Key);
+
+            return udi;
+        }
+    }
+}
